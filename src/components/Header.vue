affdc20badbdce09c8c00c35568ab73b4fe3ef7b
davidemontagna <davidemontagna92@gmail.com>
2022-02-08 13:04:54 +0100

    start (nav-top e logo inseriti)

---
+++ src/components/Header.vue
@@ -0,0 +1,44 @@
+<template>
+    <header>
+        <div class="bg-top-header d-flex align-items-center">
+            <top-nav />
+        </div>
+        <div class="container my-4">
+            <div class="row">
+                <div class="col">
+                    <div class="dm-logo">
+                        <img src="../assets/img/logos/logo-default-slim.png" alt="">
+                    </div>
+                </div>
+            </div>
+        </div>
+        
+        
+    </header>
+</template>
+
+<script>
+import TopNav from "./commons/TopNav.vue"
+
+export default {
+    name: 'Header',
+    components: {
+        TopNav
+    }
+}
+</script>
+
+<style lang="scss">
+.bg-top-header{
+    background-color: #0088cc;
+    height: 50px;    
+    width: 100vw;
+}
+
+.dm-logo{
+    img{
+        height: 60px;
+        
+    }
+}
+</style>
---
+++ src/components/commons/TopNav.vue
@@ -0,0 +1,54 @@
+<template>
+    <nav class="container">
+        <div class="d-flex justify-content-start">
+            <span>Everything about Lifestyle, Travel and Gadgets!</span>             
+                
+            <div class="dm-nav-top d-flex justify-content-evenly">
+                <a 
+                v-for="element in topNav"
+                :key="element"
+                class="dm-nav-links" 
+                href="#">
+                {{element}}
+                </a>                        
+            </div>
+                
+        </div>
+    </nav>
+</template>
+
+<script>
+export default {
+    name: 'TopNav',
+    data(){
+        return{
+            topNav: ['sign in', 'about us', 'contact us', 'buy now']
+        }
+    }
+}
+</script>
+
+<style lang="scss">
+.container{
+    span{
+        color: #b7b7b7;
+    }
+}
+
+.dm-nav-top{
+    width: 30%;
+    margin-left: 20px;
+    .dm-nav-links{
+        text-transform: uppercase;
+        color: #fff;
+        text-decoration: none;
+
+        &:hover{
+            color: #b7b7b7;
+        }        
+    }
+    
+}
+
+
+</style>
