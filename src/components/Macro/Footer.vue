a53468d8e6a7ec37e733afadd4e4d8385113e286
davidemontagna <davidemontagna92@gmail.com>
2022-02-10 10:37:43 +0100

    Parte top del footer fatta

--- src/components/Macro/Footer.vue
+++ src/components/Macro/Footer.vue
@@ -1,9 +1,14 @@
 <template>
     <footer>
         <div class="dm-foot-top">
-            <div class="container">
+            <div class="container position-relative">
                 <div class="row w-100">
                     <div class="col-12 col-sm-6 col-lg-3 w-100">
+                        
+                        <div >
+                            <div class="d-inline" id="triangle-up"></div>
+                            <span class="dm-get-in-touch">Get in Touch</span>
+                        </div>
                         <foot-top />
                     </div>
                 </div>
@@ -29,5 +34,24 @@ export default {
 
 .dm-foot-top{
     background-color: $bg-color-7;
+}
+
+.dm-get-in-touch{
+    background-color: $bg-color-5;
+    padding: 10px 15px;
+    color: $font-color-1!important;
+    font-family: $secondary-font;
+    font-size: 14px;
+}
+
+#triangle-up {
+    position: absolute;
+    top: -9px;
+    left: 2px;
+    width: 0;
+    height: 0;
+    border-left: 10px solid transparent;
+    border-right: 0px solid transparent;
+    border-bottom: 9px solid $bg-color-6;
 }
 </style>
--- src/components/commons/FootTop.vue
+++ src/components/commons/FootTop.vue
@@ -1,5 +1,5 @@
 <template>
-    <section class="d-flex justify-content-between my-5">
+    <section class="d-flex justify-content-between my-5 pb-5">
         <div class="dm-about">
             <h6 class="text-uppercase fw-bold">About the blog</h6>
             <div class="my-3">
@@ -7,7 +7,7 @@
                 <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perferendis doloremque accusantium, est aliquam cupiditate laborum atque, dolore natus velit numquam deleniti. Mollitia, pariatur.</p>
                 
             </div>
-            <h6 class="text-uppercase"><a href="#">View More <i class="fa-solid fa-chevron-right"></i></a></h6>
+            <h6 class="dm-view-more fw-bold text-uppercase"><a href="#">View More <i class="fa-solid fa-chevron-right"></i></a></h6>
         </div>
 
         <div class="dm-recent-pst mx-5">
@@ -18,7 +18,7 @@
                 </div>
                 <div class="fw-bold ms-3 dm-recent-txt">
                     <p>{{post.text}}</p>
-                    <span>{{post.date}}</span>
+                    <span class="fw-light">{{post.date}}</span>
                 </div>
             </div>
         </div>
@@ -26,9 +26,24 @@
         <div class="dm-recent-comments">
             <h6 class="text-uppercase fw-bold">Recent comments</h6>
             <div class="my-3" v-for="(comment, index) in recentComments" :key="'comm'+index">
-                <p><i class="fa-solid fa-chevron-right"></i> {{comment.author}} commented on <a href="#">{{comment.link}}</a> </p>
-                <span>{{comment.date}}</span>
+                <p>
+                    <span class="dm-arrow-rec-comments"><i class="fa-solid fa-chevron-right"></i></span>
+                    {{comment.author}}
+                    <span>commented on </span> 
+                    <a class="fw-bold" href="#">{{comment.link}}</a>
+                </p>
+                <span class="dm-rec-comm-date">{{comment.date}}</span>
             </div>
+        </div>
+
+        <div class="dm-categories ms-5">
+            <h6 class="text-uppercase fw-bold">Categories</h6>
+            <ul class="d-flex justify-content-start flex-wrap my-3">
+                <li class="dm-category-list me-2 mt-3" v-for="(category, index) in categories" :key="'cat'+index">
+                    <a href="#">{{category}}</a>
+                </li>
+                
+            </ul>
         </div>
     </section>
 </template>
@@ -61,7 +76,8 @@ export default {
                     link: 'Lorem ipsum dolor sit amet.',
                     date: '12:55 AM Dec 19th'
                 }
-            ]
+            ],
+            categories:['Gadgets', 'Photography', 'Lifestyle', 'Fashion', 'Recipes', 'Travel', 'Business', 'Architecture', 'Reviews', 'Sports', 'Videos', 'Technology', 'Design'],
         }
     }
 }
@@ -73,6 +89,7 @@ export default {
 .dm-about{
     width: 35%;
     div p{
+        font-size: 13px;
         color: $font-color-5;
     }
 }
@@ -82,7 +99,29 @@ export default {
 }
 
 .dm-recent-comments{
-    widows: 25%;
+    width: 25%;
+
+    div{
+        p{
+            font-size: 13px;
+            color: $font-color-1;
+            a{
+                text-decoration: none;
+                color: $font-color-4;
+            }
+            
+        }
+    }
+}
+
+.dm-rec-comm-date{
+    color: $font-color-5!important;
+    font-size: 11px!important;
+}
+
+.dm-arrow-rec-comments{
+    color: $font-color-4!important;
+    font-size: 11px!important;
 }
 
 .dm-recent-img{
@@ -103,11 +142,49 @@ h6{
     p{
         margin-bottom: 0;
         color: $font-color-2;
-        font-size: 15px;
+        font-size: 13px;
     }
     span{
         color: $font-color-5;
+        font-size: 11px;
+    }
+}
+
+.dm-categories{
+    width: 20%;
+
+    ul{
+        padding: 0;
+    }
+}
+
+.dm-category-list{
+    list-style: none;
+    
+
+    a{
+        padding: 6px;
+        text-decoration: none;
+        background-color: $bg-color-13;
+        border-radius: 5px;
+        color: $font-color-1;
         font-size: 13px;
+        font-weight: bold;
+
+        &:hover{
+            color: $font-color-4;
+        }
+    }
+}
+
+.dm-view-more{
+    a{
+        color: $font-color-1;
+        text-decoration: none;
+
+        &:hover{
+            color: $font-color-4;
+        }
     }
 }
 </style>
